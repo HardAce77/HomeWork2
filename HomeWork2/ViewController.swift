a0120e4077bd76eaa8741e4cddcc325d967f10db
Nikita Neluybin <nelub77@icloud.com>
2021-03-26 20:39:34 +0500

    Initial Commit

---
+++ HomeWork2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2
+//
+//  Created by Nikita Neluybin on 26.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
