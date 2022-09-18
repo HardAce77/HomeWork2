d8a181217e765aa0489b0eadabc0a507d8d9546c
Nikita <nelub77@icloud.com>
2022-09-19 00:50:30 +0500

    Work done!

--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    let flashOff: CGFloat = 0.3
+    let flashOn: CGFloat = 1
+    enum CurrentLight {
+    case red, yellow, green
+    }
+    var currentLight = CurrentLight.red
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha = flashOff
+        yellowView.alpha = flashOff
+        greenView.alpha = flashOff
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        redView.layer.cornerRadius = redView.layer.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.layer.frame.height / 2
+        greenView.layer.cornerRadius = greenView.layer.frame.height / 2
+        startButton.layer.cornerRadius = 15
+    }
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            redView.alpha = flashOn
+            yellowView.alpha = flashOff
+            greenView.alpha = flashOff
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = flashOff
+            yellowView.alpha = flashOn
+            greenView.alpha = flashOff
+            currentLight = .green
+        case .green:
+            redView.alpha = flashOff
+            yellowView.alpha = flashOff
+            greenView.alpha = flashOn
+            currentLight = .red
+        }
+    }
 }
 
